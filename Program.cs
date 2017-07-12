26f0b4f38e7fb825477d1a77fdf58916af23c298
rsakie <rsakie@yahoo.com>
2017-07-12 15:27:43 -0400

    practice Problems

--- Program.cs
+++ Program.cs
@@ -42,8 +42,72 @@ namespace Dictionaries
             //    Console.WriteLine(drink.Value);
             //}
 
+            //create a dictionary for a theater coat check 10 elements
+            //the key will be a number and value will be the coat style
+            //print all the elements to the console
+            //Dictionary<int, string> coatCheck = new Dictionary<int, string>();
 
 
+            //coatCheck.Add(1, "Winter");
+            //coatCheck.Add(2, "Summer");
+            //coatCheck.Add(3, "Spring");
+            //coatCheck.Add(4, "Tremch");
+            //coatCheck.Add(5, "Rain");
+            //coatCheck.Add(6, "Sport");
+            //coatCheck.Add(7, "None");
+            //coatCheck.Add(8, "Sleeveless");
+            //coatCheck.Add(9, "Patch Pocket");
+            //coatCheck.Add(10, "Lapel");
+
+            //foreach (KeyValuePair<int, string> item in coatCheck)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //car valet with 10 cars the key will be a customers last name and the
+            //value will be the car make. print all the elements to the console
+
+            //Dictionary<string, string> Valet = new Dictionary<string, string>()
+            //{
+            //    {"Sakie", "Dodge" },
+            //    {"Kissane", "Chevy" },
+            //    {"Lonjak", "Ford" },
+            //    {"Johnson", "Kia" },
+            //    {"Ruff", "Mazda" },
+            //    {"Sum", "Mercedes" },
+            //    {"Smith","Chevy" },
+            //    {"Jordan","Subaru" },
+            //    {"Daniels","Jeep" },
+            //    {"Cintron","Jaguar" }
+            //};
+            //foreach (KeyValuePair<string, string> car in Valet)
+            //{
+            //    Console.WriteLine(car);
+            //}
+
+            //creat a dictionary of 10 zoo animals the key will be the animal type
+            //the value will be the number of animals
+            //print the animal with the highest quantity to the console
+            //remove the zoo animalwith the lowest count
+
+            Dictionary<string, int> zooAnimals = new Dictionary<string, int>();
+
+            zooAnimals.Add("Elephant", 1);
+            zooAnimals.Add("Camel", 2);
+            zooAnimals.Add("Wolf", 3);
+            zooAnimals.Add("Shark", 4);
+            zooAnimals.Add("Polar Bear", 5);
+            zooAnimals.Add("Flamingo", 6);
+            zooAnimals.Add("Hippo", 7);
+            zooAnimals.Add("Lion", 8);
+            zooAnimals.Add("Tiger", 9);
+            zooAnimals.Add("Bear", 10);
+
+            foreach (KeyValuePair<string, int> animal in zooAnimals)
+            {
+                Console.WriteLine("Key: {0}, Value {1}", zooAnimals.Key, zooAnimals.Value);
+                Console.WriteLine(animal.Last);
+            }
         }
     }
 }
