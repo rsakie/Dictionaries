6cad49b75cf64ee1563cce0648a7ea33a9f3eea2
rsakie <rsakie@yahoo.com>
2017-07-12 14:19:09 -0400

    dictionary practice

--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
+            // syntax for dictionary
+
+            //Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            //{
+            //    {"A1","Twix" },
+            //    {"A2", "Twizzler" },
+            //    {"A3", "Reeses" },
+            //    {"A4","Sour Patch Kids"}
+
+            //};
+            //foreach (KeyValuePair<string,string>item in snackMachine)
+            //{
+            //   // Console.WriteLine(item);
+            //}
+
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+
+            ////using add method to add drinks
+            //drinkMachine.Add(10, "Ginger Ale");
+            //drinkMachine.Add(11, "Bottled Water");
+            //drinkMachine.Add(12, "Orange Juice");
+            //drinkMachine.Add(13, "Fruit Punch");
+            //drinkMachine.Add(14, "Mountain Dew Code Red");
+            ////counting the number of entries in the dictionary
+            //// Console.WriteLine(drinkMachine.Count);
+
+            //foreach (KeyValuePair<int, string> drink in drinkMachine)
+            //{
+            //   // Console.WriteLine(drink.Key);
+            //    Console.WriteLine(drink.Value);
+            //}
+
+
+
         }
     }
 }
